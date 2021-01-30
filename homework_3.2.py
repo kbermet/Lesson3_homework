679b206fb91dec1a4e1ac1c7d9b9d1ffdf9786f8
kbermet <kbermet@gmail.com>
2021-01-30 21:52:10 +0600

    Initial commit

---
+++ homework_3.2.py
@@ -0,0 +1,17 @@
+# 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия,
+# год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+
+name = input('Enter your name: ')
+surname = input('Enter your surname: ')
+birth = input('Enter your birth date: ')
+city = input('Enter your city: ')
+email = input('Enter your email: ')
+tel = input('Enter your telephone: ')
+
+
+def prof (**kwargs):
+    return prof()
+
+print (f"You are {name} {surname}, born on {birth} in {city}, your email is {email} and tel:{tel}")
+
---
+++ homework_3.3.py
@@ -0,0 +1,16 @@
+# Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших
+# двух аргументов\
+
+
+def my_func():
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
+    num3 = int(input('Enter third number: '))
+
+    return max(my_func)
+
+
+print(my_func())
+
+
+
